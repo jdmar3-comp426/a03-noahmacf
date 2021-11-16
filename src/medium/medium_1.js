bb629364afb6588d78abfa6fbf6025bd65b7d322
Noah MacFarlane <noahmacf@gmail.com>
2021-11-16 10:27:01 -0500

    first upload

--- src/medium/medium_1.js
+++ src/medium/medium_1.js
@@ -76,3 +76,12 @@ export function getStatistics(array) {
     return statistics;
 }
 
+export function variance(array, mean) {
+    return array.map(function (sample) {
+        return Math.pow(mean - sample, 2);
+    })
+        .reduce(function sum(m, v) {
+            m += v;
+            return m;
+        }, 0) / array.length;
+}
